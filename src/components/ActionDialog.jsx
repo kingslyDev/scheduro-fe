c23d6fff6520e95793cd10a3f69381446594d773
kingslydev <ghalyallcoc@gmail.com>
2025-03-09 01:45:14 +0700

    salah penulisan bro

--- src/components/ActionDialog.jsx
+++ src/components/ActionDialog.jsx
@@ -1,4 +1,4 @@
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/Components/ui/alert-dialog';
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 
 export function ActionDialog({ trigger, action, title, description }) {
   return (
--- src/components/GetPriorityBadge.jsx
+++ src/components/GetPriorityBadge.jsx
@@ -1,4 +1,4 @@
-import { Badge } from '@/Components/ui/badge';
+import { Badge } from '@/components/ui/badge';
 import { PRIORITY } from '@/lib/utils';
 
 export default function GetPriorityBadge({ priority }) {
