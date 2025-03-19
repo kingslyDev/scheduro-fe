102d3cfd991d32d8e5b09821de57d9e9bd1e8c4f
Dio Ramadhan <dioramadhan9@gmail.com>
2025-03-19 12:27:05 +0700

    responsive left sidebar test

--- src/components/layouts/AppLayout.jsx
+++ src/components/layouts/AppLayout.jsx
@@ -25,13 +25,26 @@ export default function AppLayout({ children, title }) {
 
   return (
     <>
-      <Head title={title} />
+      <Head>
+  <title>{title ? `${title} - Scheduro` : 'Scheduro'}</title>
+</Head>
+
       <Toaster position="top-center" richColors />
       <div>
+        {/* SIDEBAR MOBILE */}
         <Transition.Root show={sidebarOpen} as={Fragment}>
-          <Dialog as="div" className="relative z-50 lg:hidden" onClose={setSidebarOpen}>
-            <Transition.Child as={Fragment} enter="transition-opacity ease-linear duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="transition-opacity ease-linear duration-300" leaveFrom="opacity-100" leaveTo="opacity-0">
-              <div className="fixed inset-0 bg-gray-900/80" />
+          <Dialog as="div" className="fixed inset-0 z-50 lg:hidden" onClose={setSidebarOpen}>
+            {/* Overlay gelap */}
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black/50" />
             </Transition.Child>
 
             <div className="fixed inset-0 flex">
@@ -44,35 +57,38 @@ export default function AppLayout({ children, title }) {
                 leaveFrom="translate-x-0"
                 leaveTo="-translate-x-full"
               >
-                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-                  <Transition.Child as={Fragment} enter="ease-in-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in-out duration-300" leaveFrom="opacity-100" leaveTo="opacity-0">
-                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                      <button type="button" className="-m-2.5 p-2.5" onClick={() => setSidebarOpen(false)}>
-                        <span className="sr-only">Close sidebar</span>
-                        <PiX className="h-6 w-6 text-white" aria-hidden="true" />
-                      </button>
-                    </div>
-                  </Transition.Child>
-                  <Sidebar auth={auth} url={url} workspaces={workspaces} />
+                {/* Sidebar */}
+                <Dialog.Panel className="relative w-[80%] max-w-xs bg-white dark:bg-gray-900 shadow-lg">
+                  {/* Tombol Close */}
+                  <div className="absolute right-2 top-2">
+                    <button type="button" className="p-2 text-white" onClick={() => setSidebarOpen(false)}>
+                      <PiX className="h-6 w-6" aria-hidden="true" />
+                    </button>
+                  </div>
+
+                  {/* Sidebar Content */}
+                  <div className="h-full overflow-y-auto p-4">
+                    <Sidebar auth={auth} url={url} workspaces={workspaces} />
+                  </div>
                 </Dialog.Panel>
               </Transition.Child>
             </div>
           </Dialog>
         </Transition.Root>
 
+        {/* SIDEBAR DESKTOP */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:border-gray-800 dark:bg-gray-900">
             <div className="flex h-16 shrink-0 items-center justify-center space-x-1.5">
-              {' '}
-              {/* Centering added here */}
               <Link href="/" className="-m-1.5 p-1.5 mt-5">
-                <img src="https://res.cloudinary.com/dwgwb5vro/image/upload/v1741442563/sidebarlogo_xx2gbf.png" alt="Plannify logo" className="inline-block w-auto h-8" />
+                <img src="https://res.cloudinary.com/dwgwb5vro/image/upload/v1741442563/sidebarlogo_xx2gbf.png" alt="Scheduro logo" className="inline-block w-auto h-8" />
               </Link>
             </div>
             <Sidebar auth={auth} url={url} workspaces={workspaces} />
           </div>
         </div>
 
+        {/* NAVBAR MOBILE */}
         <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm dark:bg-gray-900 sm:px-6 lg:hidden">
           <button type="button" className="-m-2.5 p-2.5 text-gray-700 lg:hidden" onClick={() => setSidebarOpen(true)}>
             <span className="sr-only">Open sidebar</span>
@@ -87,6 +103,7 @@ export default function AppLayout({ children, title }) {
           </Link>
         </div>
 
+        {/* CONTENT AREA */}
         <main className="py-10 lg:pl-72">
           <div className="px-4 sm:px-6 lg:px-8">{children}</div>
         </main>
